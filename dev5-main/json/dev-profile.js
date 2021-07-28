e89af4593e5c082cc2698396143fec360a1ffbe2
Whitehawk2015 <85392086+Whitehawk2015@users.noreply.github.com>
2021-07-28 17:39:43 -0500

    Add files via upload

---
+++ dev5-main/json/dev-profile.js
@@ -0,0 +1,10 @@
+myFunction([
+    {
+    "display":"Name: Gavin King"},{
+    "display":"Github: Whitehawk2015",
+    "url": "https://github.com/Whitehawk2015"},{
+    "display":"Background: UNA: BBA CIS 2021"},{
+    "display":"Interests: Psychology, Comedy, Music"},{
+    "display":"Must Know Dev Tech: Use Firefox for testing HTML & CSS, Chrome for JS"
+}
+])
